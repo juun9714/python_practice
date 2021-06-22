b3e51881ad1134bcf83ed248c4007089c031b254
juun9714 <lovekwon200@gmail.com>
2021-06-22 18:50:13 +0900

    sw test

--- test.py
+++ test.py
@@ -1,58 +1,15 @@
-# 47차시 5. 객체지향 - 연습문제 4
-# class Circle:
-#     PI=3.14
-#     def __init__(self, half):
-#         self.half=half
-    
-#     def space(self):
-#         return pow(self.half,2)*self.PI
+t=int(input())
+big=[]
+for i in range(t):
+    line=input()
+    tmp=[]
+    for k in range(10):
+        tmp.append(int(line.split(" ")[k]))
+    big.append(tmp)
 
-# cir=Circle(2)
-# print("원의 면적: %0.2f"%cir.space())
-
-# 48차시 5. 객체지향 - 연습문제 5
-
-# class Rectangle:
-#     def __init__(self, width, height):
-#         self.width=width
-#         self.height=height
-
-#     def square(self):
-#         return self.width*self.height
-
-# r=Rectangle(4, 5)
-# print("사각형의 면적: %d"%r.square())
-
-# 49차시 5. 객체지향 - 연습문제 6
-
-# class Shape:
-#     def area():
-#         return 0
-
-# class Square(Shape):
-#     def __init__(self, length):
-#         self.length=length
-#     def area(self):
-#         return pow(self.length,2)
-
-# s=Square(3)
-# print("정사각형의 면적: {0}".format(s.area()))
-
-# 50차시 5. 객체지향 - 연습문제 7
-
-class Person:
-    def getGender():
-        return "Unknown"
-
-class Male(Person):
-    def getGender(self):
-        return "Male"
-
-class Female(Person):
-    def getGender(self):
-        return "Female"
-
-m=Male()
-f=Female()
-print(m.getGender())
-print(f.getGender())
+for k in range(t):
+    sum=0
+    for j in range(10):
+        if big[k][j]%2==1:
+            sum+=big[k][j]
+    print("#{0} {1}".format(k+1,sum))
