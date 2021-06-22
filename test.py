6405a689d0d12feb441ca1bd35445d91a0a72471
juun9714 <lovekwon200@gmail.com>
2021-06-22 13:44:41 +0900

    24

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
-#20차시 2. 자료구조 – 리스트, 튜플 - 연습문제 22
-line=[5, 6, 77, 45, 22, 12, 24]
-new=[line[k] for k in range(len(line)) if line[k]%2==1]
-print(new)
+#22차시 2. 자료구조 – 리스트, 튜플 - 연습문제 24
+result=[]
+for i in range(2):
+    tmp1=[]
+    for j in range(3):
+        tmp2=[]
+        for k in range(4):
+            tmp2.append(0) #append zero
+        tmp1.append(tmp2)
+    result.append(tmp1)
+    
+print(result)
