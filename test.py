1a68436028eb770e64b6b5430c03693657f08d76
juun9714 <lovekwon200@gmail.com>
2021-06-22 13:39:37 +0900

    22

--- test.py
+++ test.py
@@ -1,28 +1,4 @@
-# 17차시 2. 자료구조 – 리스트, 튜플 - 연습문제 19
-# line = input()
-# lines=list(line.split(", "))
-# new=sorted(lines)
-
-# for k in range(0, len(new)):
-#     if k==len(new)-1:
-#         print(new[k])
-#     else:
-#         print(new[k],end=", ")
-
-#18차시 2. 자료구조 – 리스트, 튜플 - 연습문제 20
-
-#split
-# line=input()
-# lines=list(map(int,line.split(", ")))
-
-# new=[k for k in lines if k%2==1]
-# print(str(new)[1:-1])
-
-#19차시 2. 자료구조 – 리스트, 튜플 - 연습문제 21
-
-t=(1,2,3,4,5,6,7,8,9,10)
-t1=t[:(len(t)//2)]
-t2=t[(len(t)//2):]
-
-print(t1)
-print(t2)
+#20차시 2. 자료구조 – 리스트, 튜플 - 연습문제 22
+line=[5, 6, 77, 45, 22, 12, 24]
+new=[line[k] for k in range(len(line)) if line[k]%2==1]
+print(new)
