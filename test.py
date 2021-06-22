50981f4f209a294daf96cf021d346b287728c329
juun9714 <lovekwon200@gmail.com>
2021-06-22 13:28:37 +0900

    21

--- test.py
+++ test.py
@@ -12,8 +12,17 @@
 #18차시 2. 자료구조 – 리스트, 튜플 - 연습문제 20
 
 #split
-line=input()
-lines=list(map(int,line.split(", ")))
+# line=input()
+# lines=list(map(int,line.split(", ")))
 
-new=[k for k in lines if k%2==1]
-print(str(new)[1:-1])
+# new=[k for k in lines if k%2==1]
+# print(str(new)[1:-1])
+
+#19차시 2. 자료구조 – 리스트, 튜플 - 연습문제 21
+
+t=(1,2,3,4,5,6,7,8,9,10)
+t1=t[:(len(t)//2)]
+t2=t[(len(t)//2):]
+
+print(t1)
+print(t2)
